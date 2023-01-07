68ba7c5d9b49f3a439eb4d40968939d22d60f019
Dallas Palumbo <palumbodallas@gmail.com>
2023-01-07 00:32:58 -0500

    getting up and running

---
+++ src/testingapi.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+
+function TestApi() {
+
+const [items , setitems] = useState([])
+
+useEffect(()=>{
+   const getData = async() =>{
+   const resdata = await fetch('https://fakestoreapi.com/products/1')
+   const parsedData = await resdata.json()
+   setitems(parsedData)
+   window.localStorage.setItem('test',JSON.stringify(parsedData))
+   }
+
+   getData()
+},[])
+ console.log(items)
+  return (
+    <div>
+        <img src={items.image} alt={items.title}/>  
+    </div>
+  )
+}
+
+export default TestApi
