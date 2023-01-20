cb522aea44ebe95a3ebcb7f89e660ff246c2d29a
Dallas Palumbo <palumbodallas@gmail.com>
2023-01-20 13:35:10 -0500

    styled web home

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,6 +1,6 @@
-import { Button, Paper } from '@mui/material'
+import { Box, Button, Paper, Stack, Typography } from '@mui/material'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Carousel from 'react-material-ui-carousel'
 
 import photo1 from "../assets/photo1.jpg"
@@ -29,10 +29,23 @@ const PhotoGallery = () => {
   )
 }
 
+const hot = <Box bgcolor="rgb(255, 102, 102,0.5)" border={1} borderRadius="10px">
+<Typography color={"#fff"} fontSize={"12rem"} fontFamily="Optima" sx={{textShadow:"1px 4px 2px rgba(0,0,0,0.6)"}}>
+    Hot Deals
+</Typography>
+</Box>
+
+const cool = <Box bgcolor="rgb(102, 179, 255,0.5)">
+<Typography color={"#fff"} fontSize={"12rem"} fontFamily="Century Gothic" sx={{textShadow:"1px 4px 2px rgba(0,0,0,0.6)"}}>
+    Cool styles
+</Typography>
+</Box>
+
 export default PhotoGallery
 
-function Item({item})
-{
+function Item({item}){
+
+
     return (
         <Paper sx={{
             backgroundImage:`url(${item})`,
@@ -45,12 +58,9 @@ function Item({item})
             
 
         }}>
-            <h2>working</h2>
-            <p>huh</p>
-
-            <Button className="CheckButton">
-                Check it out!
-            </Button>
+        <Stack minHeight={"100vh"} alignItems="flex-end" justifyContent={"center"} >
+            {hot}
+        </Stack>
         </Paper>
     )
 }
--- src/components/VideoHome.jsx
+++ src/components/VideoHome.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 
 import React from 'react'
 
@@ -8,28 +8,31 @@ import BgVideo from "../assets/landingbg.mp4"
 
 const HomeVideo = () => {
 
+ const cool = <Box bgcolor="rgb(102, 179, 255,0.5)" border={1} borderRadius="10px">
+<Typography color={"#fff"} fontSize={"12rem"} fontFamily="Verdana" sx={{textShadow:"1px 4px 2px rgba(0,0,0,0.6)"}}>
+    Cool Styles
+</Typography>
+</Box>
+
   return (
     <Box  >
-    <Box width="100%" height="100%">
+    <Box zIndex={-2} position={"absolute"} width="100%" height="100%">
         <video autoPlay loop muted
         style={{
             width:"100%",
             maxHeight:"100vh",
             objectFit:"cover",
             zIndex:"-1",
+            position:"absolute"
          
 
         }}
         >
             <source src={BgVideo} type="video/mp4"/>
         </video>
-        {/* <Stack height="100%" justifyContent="center" alignItems="center">
-            <Box width="20%" bgcolor="green">
-                <Typography>
-                    Shop the hottest fashons
-                </Typography>
-            </Box>
-        </Stack> */}
+        <Stack zIndex={3}  minHeight="100vh" justifyContent="center" alignItems="flex-start">
+            {cool}
+        </Stack>
     </Box> 
     </Box>
   )
